8fc0b15f7a6946ac7d02ffa123c85acd9c9eff35
Sean Alden <sa2596491@gmail.com>
2026-02-19 17:51:20 +0700

    add path

--- src/router/index.js
+++ src/router/index.js
@@ -180,7 +180,7 @@ const routes = [
         path: "/shippingmanagement",
         name: "ShippingManagementPage",
         component: ShippingManagementPage,
-        meta: { requiresAuth: true, hideHeaderFooter: false, isAdmin: true }
+        meta: { requiresAuth: true, hideHeaderFooter: true, isAdmin: true }
     }
 ]
 
