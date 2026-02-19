0ad7d0149bd08bc4d5a108cff7d9856ed2b30367
Sean Alden <sa2596491@gmail.com>
2026-02-19 13:33:39 +0700

    update sales report page (4)

--- src/components/Admin/SalesReportPage.vue
+++ src/components/Admin/SalesReportPage.vue
@@ -416,16 +416,11 @@ onMounted(fetchReport);
             >
               <td class="py-4">
                 <div class="flex items-center gap-4">
-                  <!-- <img
-                    :src="item.image"
-                    alt="Product"
-                    class="bg-gray-100 shadow-sm rounded-lg w-12 h-12 object-cover"
-                    crossorigin="anonymous"
-                  /> -->
                   <img
                     :src="item.image"
                     alt="Product"
                     class="bg-gray-100 shadow-sm rounded-lg w-12 h-12 object-cover"
+                    crossorigin="anonymous"
                   />
                   <div>
                     <p class="font-bold text-gray-900">{{ item.name }}</p>
