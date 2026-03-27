af5e9298cf888d3f5c90529337ae20e7c19511df
Sean Alden <sean.alden@great.sch.id>
2026-03-27 15:44:25 +0700

    add promo pop-up in home page #12

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://127.0.0.1:8000/api";
 // export const BASE_URL = "https://backend-solherbag.vercel.app/api/api";
 
-export const BASE_URL = "http://31.97.60.207:8246/api";
+// export const BASE_URL = "http://31.97.60.207:8246/api";
+export const BASE_URL = "http://solher.co.id/api";
