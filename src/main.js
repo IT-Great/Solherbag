1680bdeaaf1837b5fbb80d74fe4bfae15c7f77d0
Sean Alden <sean.alden@great.sch.id>
2026-04-25 14:10:52 +0700

    update header & chat list page #4

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // Import router
 
+// [PERBAIKAN KRITIS] Panggil file bootstrap/echo Anda di sini!
+import './echo'; // Atau import './echo' (Tergantung nama file konfigurasi Pusher Anda)
+
 const app = createApp(App)
 app.use(router) // Gunakan router
 app.mount('#app')
