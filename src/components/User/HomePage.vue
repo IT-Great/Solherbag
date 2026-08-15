ae53fb75ea31e5c8a2c06ce96b0ba09f5016e78c
Sean Alden <sean.alden@great.sch.id>
2026-08-15 15:44:59 +0700

    update home page pop-up

--- src/components/User/HomePage.vue
+++ src/components/User/HomePage.vue
@@ -5751,9 +5751,9 @@ import { useProductStore } from "../../composables/useProductStore";
 import Swal from "sweetalert2";
 import { useI18n } from "vue-i18n";
 
-import banner1 from "../../assets/first_banner.png";
-import banner2 from "../../assets/second_banner.png";
-import banner3 from "../../assets/third_banner.jpg";
+import banner2 from "../../assets/first_banner.png";
+import banner3 from "../../assets/second_banner.png";
+import banner1 from "../../assets/third_banner.jpg";
 
 // 👇 KONFIGURASI TIPE POPUP (Ganti 'merdeka' ke 'default' untuk mengembalikan ke popup awal) 👇
 const ACTIVE_POPUP = ref("merdeka");
